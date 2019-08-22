ef68a67836a17d4f368319f083cc43bb77f6203b
wm00144 <wangmeng02@ilarge.cn>
2019-08-22 21:26:19 +0800

    11

---
+++ wmcloud-job-server/src/test/java/com/wmcloud/job/WmcloudJobServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wmcloud.job;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WmcloudJobServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
